b5372168d8ea5db9dd11ef0da579930f7168b2d3
Armin <arminch1997@gmail.com>
2021-05-25 17:32:11 -0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //let key = '7737f57b4924159f2e353b4654856253'
 let savedCity = JSON.parse(localStorage.getItem('saved')) || []
 for (let i=0; i<savedCity.length; i++){
-  $('#hist').append(`<div data-value='${savedCity[i]}' class="border row align-items-start"><h3>${savedCity[i]}</h3></div>`)
+  $('#hist').append(`<div data-name="${savedCity[i]}" class="border row align-items-start savedCity"><h3>${savedCity[i]}</h3></div>`)
 }
 document.getElementById('search').addEventListener('click', function (event) {
   event.preventDefault()
@@ -20,32 +20,33 @@ document.getElementById('search').addEventListener('click', function (event) {
   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=7737f57b4924159f2e353b4654856253`)
   .then(res => {
     console.log(res.data)
+    $('h1').append(`<img src="http://openweathermap.org/img/wn/${res.data.weather[0].icon}.png">`)
     $('#main').append(`<p>Temperature: ${res.data.main.temp} F</p>
     <p>Humidity: ${res.data.main.humidity}%</p>
     <p>Wind Speed: ${res.data.wind.speed} MPH</p>
     <p>Feels Like: ${res.data.main.feels_like} F</p>
     `)
+    $('#main').append(`<h2>4-Day Forecast</h2> <div class="container">
+     <div id='forecast' class="row"> </div> </div>`)
     document.getElementById('city').value = ''
     axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${res.data.coord.lat}&lon=${res.data.coord.lon}&units=imperial&appid=7737f57b4924159f2e353b4654856253`)
     .then(res2=>{
       console.log(res2.data)
-      $('#main').append(`
-     <h2>5-Day Forecast</h2>
-     <div class="container">
-     <div class="row">
-    <div class="col-sm-4">${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}
-    </div>
-    <div class="col-sm-4">Column</div>
-    <div class="col-sm-4">Column</div>
-    <div class="col-sm-4">Column</div>
-  </div>
-</div>
-
-    `)
+      for(let i=0; i<4;i++){
+      $('#forecast').append(`
+      <div class="col-sm-3 border bg-primary"><p>${date.getMonth() + 1}/${date.getDate()+i+1}/${date. getFullYear()}</p>
+      <p><img src="http://openweathermap.org/img/wn/${res2.data.daily[i].weather[0].icon}.png"></p>
+      <p>Temp: ${res2.data.daily[i].temp.day} F</p>
+      <p>Humidity: ${res2.data.daily[i].humidity} %</p>
+      </div>`)
+      }
     })
     .catch(err => console.error(err))
   })
     .catch(err => console.error(err))
+})
+$('.savedCity').click(function (event) {
+  document.getElementById('city').value = this.dataset.name
 })
 
 // $('#search').click(displayForecast())
